cd241bfaa2145d11fceb807c6e0005b7e43f35fa
dekpiano <dekpiano4815@gmail.con>
2021-02-15 16:20:37 +0700

    15/02/64 เพิ่มแบนเนอร์สมัคร

---
+++ application/views/user/popupSpecial/skj-640215-Vlogger.php
@@ -0,0 +1,56 @@
+<style>
+/* // Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+    .Vlogger-setion1{
+        padding-top: 110px;
+    }
+    .Vlogger-setion1 h1{
+        font-size: 2rem;
+    }
+    .Vlogger-setion1 h2{
+        font-size: 1.5rem;
+    }
+    .Vlogger-setion1 h3{
+        font-size: 1.3rem;
+    }
+}
+
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {}
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {}
+
+/* // Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    .skjVlogger {}
+
+    .skjVlogger h1 {
+        font-size: 3rem;
+    }
+
+}
+</style>
+
+<div class="skjVlogger" style=" background-image: linear-gradient(180deg, #52a0fd, #e8f2fd);">
+    <div class="container ">
+        <div class="row vh-100">
+            <div class="col-md-7 align-self-md-center Vlogger-setion1">
+                <div class="text-center">
+                    <h2> โครงการผลิตสื่อสังคมออนไลน์สำหรับ <br></h2>
+                    <h1><b>Youtuber & Vlogger :YVY</b> </h1>
+                    <h3>วันที่ 27 กุมภาพันธ์ ถึง 2 มีนาคม 2564</h3>
+                    <a href="https://forms.gle/ewPthDUaGmP9pnfc7" target="_blank"
+                        class="btn btn-primary">สมัครเข้าอบรม</a>
+                </div>
+            </div>
+            <div class="col-md-5 align-self-md-center ">
+                <img src="<?=base_url('uploads/banner/Vlogger/logo-Vlogger.svg');?>" alt="" class="img-fluid">
+            </div>
+
+        </div>
+    </div>
+</div>
