2d44e2d24400f442b0c4f3d20f0b7f90c8f68cc7
dekpiano <dekpiano4815@gmail.com>
2021-04-29 22:52:57 +0700

    29/04/64 เพิ่มแบนเนอร์ประกาศเลื่อน

---
+++ application/views/user/popupSpecial/skj-640429-Announcement-postponement .php
@@ -0,0 +1,63 @@
+<style>
+/* // Extra small devices (portrait phones, less than 576px) */
+@media (max-width: 575.98px) {
+    .skjAnnouncement{
+        padding-top: 80px;
+    }
+    .skjAnnouncement .content h1{
+        font-size: 4rem;
+    }
+    .skjAnnouncement .content h2{
+        font-size: 2.7rem;
+    }
+    .skjAnnouncement .content h3{
+        font-size: 1.3rem;
+    }
+    .skjAnnouncement .banner{
+        margin-bottom: -70px;}
+    }
+}
+
+/* // Small devices (landscape phones, 576px and up) */
+@media (min-width: 576px) and (max-width: 767.98px) {}
+
+/* // Medium devices (tablets, 768px and up) */
+@media (min-width: 768px) and (max-width: 991.98px) {}
+
+/* // Large devices (desktops, 992px and up) */
+@media (min-width: 992px) and (max-width: 1199.98px) {}
+
+/* // Extra large devices (large desktops, 1200px and up) */
+@media (min-width: 1200px) {
+    .skjAnnouncement {}
+
+    .skjAnnouncement h1 {
+        font-size: 5rem;
+    }
+    .skjAnnouncement h2 {
+        font-size: 3rem;
+    }
+
+}
+</style>
+
+<div class="skjAnnouncement" style=" background-image: linear-gradient(180deg, #52a0fd, #e8f2fd);">
+    <div class="container ">
+        <div class="row vh-100">
+        <div class="col-md-5 align-self-md-center banner">
+                <img src="<?=base_url('uploads/banner/64Announcement/banner.svg');?>" alt="" class="img-fluid">
+            </div>
+            <div class="col-md-7 align-self-md-center content">
+                <div class="text-center">
+                    <h1> ประกาศ <br></h1>
+                    <h2><b><u>เลื่อน</u></b> สอบคัดเลือก </h2>
+                    <h3>เดิม วันที่ 1 และ 2 พฤษภาคม 2564</h3>
+                    <h3>จนกว่าจะมีประกาศแจ้งกำหนดการครั้งใหม่<br>
+                        เนื่องด้วยสถานการณ์ระบาดของโรคไวรัสโคโรนา 19</h3>
+                </div>
+            </div>
+           
+
+        </div>
+    </div>
+</div>
